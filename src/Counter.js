a1cc89f79075468326b5a8c7b81f1930996f7d9a
Joshua Martin <the.manuscriptmaster@gmail.com>
2018-12-05 17:10:21 -0500

    Add useEffect demo

--- src/Counter.js
+++ src/Counter.js
@@ -1,76 +1,7 @@
-import React, { Component, useState, useReducer, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Counter.css';
-import counterReducer from './reducers/counter';
 
-export class CounterClass extends Component {
-  constructor(props) {
-    super(props);
-    this.backgroundColor = 'orange';
-    this.state = {
-      count: 0
-    }
-  }
-
-  _setCount = (count) => this.setState({ count });
-
-  decrementCount = () =>
-    this._setCount(this.state.count < 1 ? 0 : this.state.count - 1);
-  incrementCount = () =>
-    this._setCount(this.state.count + 1);
-  resetCount = () =>
-    this._setCount(0);
-
-  render() {
-    const { count } = this.state;
-    const { backgroundColor, decrementCount, incrementCount, resetCount } = this;
-    return (
-      <div className="counter" style={{ backgroundColor }}>
-        <h1>{count}</h1>
-        <button onClick={decrementCount}>-</button>
-        <button onClick={resetCount}>Reset</button>
-        <button onClick={incrementCount}>+</button>
-      </div>
-    );
-  }
-};
-
-export const CounterWithState = () => {
-  const [ count, setCount ] = useState(0);
-  const decrementCount = () => setCount(count < 1 ? 0 : count - 1);
-  const incrementCount = () => setCount(count + 1);
-  const resetCount = () => setCount(0);
-
-  const backgroundColor = 'aqua';
-
-  return (
-    <div className="counter" style={{ backgroundColor }}>
-      <h1>{count}</h1>
-      <button onClick={decrementCount}>-</button>
-      <button onClick={resetCount}>Reset</button>
-      <button onClick={incrementCount}>+</button>
-    </div>
-  );
-};
-
-export const CounterWithReducer = () => {
-  const [ { count }, dispatch ] = useReducer(
-    counterReducer,
-    { count: 0 }
-  );
-
-  const backgroundColor = 'greenyellow';
-
-  return (
-    <div className="counter" style={{ backgroundColor }}>
-      <h1>{count}</h1>
-      <button onClick={() => dispatch({ type: 'DECREMENT' })}>-</button>
-      <button onClick={() => dispatch({ type: 'RESET' })}>Reset</button>
-      <button onClick={() => dispatch({ type: 'INCREMENT' })}>+</button>
-    </div>
-  );
-};
-
-export const CounterWithLifecycle = () => {
+export default () => {
   const [ count, setCount ] = useState(0);
   const decrementCount = () => setCount(count < 1 ? 0 : count - 1);
   const incrementCount = () => setCount(count + 1);
