dcc9977d05f8b14bcb6e576931de4aec82b35685
Joshua Martin <the.manuscriptmaster@gmail.com>
2019-01-09 13:17:53 -0500

    Isolate useContext counter demo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import React from 'react';
 import './App.css';
-import {
-  CounterClass,
-  CounterWithState,
-  CounterWithReducer,
-  CounterWithLifecycle,
-  CounterWithContext
-} from './Counter';
-
+import CounterWithContext from './Counter';
 import CounterProvider from './contexts/CounterProvider';
 
 const App = () =>
   <div className="app">
-    <CounterClass />
-    <CounterWithState />
-    <CounterWithReducer />
-    <CounterWithLifecycle />
     <CounterProvider>
       <CounterWithContext />
     </CounterProvider>
